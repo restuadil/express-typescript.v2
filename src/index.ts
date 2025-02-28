67bc78428ab2d9c572c453cb53f3b13a3f82a7a6
restu adil <restuadil09@gmail.com>
2025-02-28 23:28:04 +0700

    aderrorMiddleware);

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express, { Application } from "express";
 import { env } from "./config/env";
 import { logger } from "./config/logger";
-import { logMiddleware } from "./middleware/log-middlewar";
-import { routeMiddleware } from "./middleware/route-middleware";
+import { logMiddleware } from "./middleware/log.middlewar";
+import { routeMiddleware } from "./middleware/route.middleware";
+import { errorMiddleware } from "./middleware/error.middleware";
 const app: Application = express();
 
 app.use(express.json());
 
 app.use(logMiddleware);
+
+app.use(errorMiddleware);
 app.use(routeMiddleware);
 
 app.get("/", (req, res) => {
